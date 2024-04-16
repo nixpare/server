9e4e0a7eaea421e2749d764a243f1d11772e6d77
Alessio Pareto (Desktop-Pare) <alessio.pareto@nixpare.com>
2024-04-16 17:11:07 +0200

    Aligned tcp server

--- v3/tcp_server.go
+++ v3/tcp_server.go
@@ -6,29 +6,23 @@ import (
 	"fmt"
 	"io"
 	"net"
-	"strings"
 
 	"github.com/nixpare/logger/v2"
 	"github.com/nixpare/server/v3/life"
 )
 
-type Conn struct {
-	TCPConn net.Conn
-	RemoteAddr string
-}
-
-type ConnHandlerFunc func(srv *TCPServer, conn *Conn)
+type ConnHandlerFunc func(srv *TCPServer, conn net.Conn)
 
 type TCPServer struct {
-	listener net.Listener
-	address string
-	port int
-	secure bool
-	Online bool
-	state *life.LifeCycle
+	listener    net.Listener
+	address     string
+	port        int
+	secure      bool
+	Online      bool
+	state       *life.LifeCycle
 	ConnHandler ConnHandlerFunc
-	Router  *Router
-	Logger  logger.Logger
+	Router      *Router
+	Logger      logger.Logger
 }
 
 func NewTCPServer(address string, port int, secure bool, certs ...Certificate) (*TCPServer, error) {
@@ -39,27 +33,27 @@ func newTCPServer(address string, port int, secure bool, certs []Certificate, l 
 	var listener net.Listener
 	var err error
 
-	listenAddr := fmt.Sprintf("%s:%d", address, port)
+	listenAddr, err := net.ResolveTCPAddr("tcp", fmt.Sprintf("%s:%d", address, port))
+	if err != nil {
+		return nil, err
+	}
+
+	listener, err = net.ListenTCP("tcp", listenAddr)
+	if err != nil {
+		return nil, err
+	}
 
 	if secure {
-		tslConfig, err := GenerateTSLConfig(certs)
+		tlsConfig, err := GenerateTSLConfig(certs)
 		if err != nil {
 			return nil, err
 		}
 
-		listener, err = tls.Listen("tcp", listenAddr, tslConfig)
-		if err != nil {
-			return nil, err
-		}
-	} else {
-		listener, err = net.Listen("tcp", listenAddr)
-		if err != nil {
-			return nil, err
-		}
+		listener = tls.NewListener(listener, tlsConfig)
 	}
 
 	if l == nil {
-		l = logger.DefaultLogger.Clone(nil, true, "server", "tcp", fmt.Sprintf("port:%d", port))
+		l = logger.DefaultLogger.Clone(nil, true, "server", "tcp", fmt.Sprint(port))
 	}
 
 	return &TCPServer {
@@ -75,44 +69,60 @@ func (srv *TCPServer) Start() {
 	if srv.state.AlreadyStarted() {
 		return
 	}
+	defer srv.state.SetState(life.LCS_STARTED)
 
 	srv.Online = true
-	srv.state.SetState(life.LCS_STARTED)
+	go srv.handler()
 
-	go func() {
-		for srv.state.GetState() == life.LCS_STARTED {
-			conn, err := srv.listener.Accept()
-			if err != nil {
-				if !errors.Is(err, net.ErrClosed) {
-					srv.Logger.Print(logger.LOG_LEVEL_ERROR, err)
-				}
-				
-				continue
+	srv.Logger.Printf(logger.LOG_LEVEL_INFO, "TCP Server %d startup completed", srv.port)
+}
+
+func (srv *TCPServer) handler() {
+	for {
+		conn, err := srv.listener.Accept()
+		if err != nil {
+			if errors.Is(err, net.ErrClosed) {
+				break
 			}
-	
-			c := createConn(conn)
-	
-			if srv.Online && srv.ConnHandler != nil {
-				go func() {
-					err := logger.PanicToErr(func() error {
-						srv.ConnHandler(srv, c)
-						return nil
-					})
-					if err != nil {
-						srv.Logger.Printf(logger.LOG_LEVEL_ERROR, "Panic captured: %v", err.Error())
-					}
-				}()
-			}
+
+			srv.Logger.Print(logger.LOG_LEVEL_ERROR, err)
+			continue
 		}
-	}()
+
+		go func() {
+			defer conn.Close()
+
+			if srv.ConnHandler == nil {
+				return
+			}
+
+			err := logger.PanicToErr(func() error {
+				srv.ConnHandler(srv, conn)
+				return nil
+			})
+			if err != nil {
+				srv.Logger.Printf(logger.LOG_LEVEL_ERROR, "Panic captured: %v", err.Error())
+			}
+		}()
+	}
 }
 
 func (srv *TCPServer) Stop() error {
 	srv.state.SetState(life.LCS_STOPPING)
-	srv.Online = false
-
 	defer srv.state.SetState(life.LCS_STOPPED)
-	return srv.listener.Close()
+
+	srv.Online = false
+	err := srv.listener.Close()
+	if err != nil {
+		srv.Logger.Printf(logger.LOG_LEVEL_FATAL,
+			"TCP Server %d shutdown failed due to: %v",
+			srv.port, err,
+		)
+	} else {
+		srv.Logger.Printf(logger.LOG_LEVEL_INFO, "TCP Server %d shutdown finished", srv.port)
+	}
+	
+	return err
 }
 
 func (srv *TCPServer) Address() string {
@@ -127,43 +137,44 @@ func (srv *TCPServer) IsSecure() bool {
 	return srv.secure
 }
 
-func createConn(conn net.Conn) *Conn {
-	remoteAdrr := strings.Split(conn.RemoteAddr().String(), ":")[0]
+func TCPPipe(conn1, conn2 net.Conn) {
+	done := make(chan struct{})
 
-	return &Conn {
-		TCPConn: conn,
-		RemoteAddr: remoteAdrr,
-	}
+	go func() {
+		defer func() {
+			conn2.Close()
+			done <- struct{}{}
+		}()
+
+		io.Copy(conn1, conn2)
+	}()
+
+	go func() {
+		defer func() {
+			conn2.Close()
+			done <- struct{}{}
+		}()
+
+		io.Copy(conn2, conn1)
+	}()
+
+	<- done
+	<- done
 }
 
-func TCPProxy(address string, port int) ConnHandlerFunc {
-	dest := fmt.Sprintf("%s:%d", address, port)
+func TCPProxy(address string, port int) (ConnHandlerFunc, error) {
+	target, err := net.ResolveTCPAddr("tcp", fmt.Sprintf("%s:%d", address, port))
+	if err != nil {
+		return nil, err
+	}
 
-	return func(srv *TCPServer, conn *Conn) {
-		proxyDest, err := net.Dial("tcp", dest)
+	return func(srv *TCPServer, conn net.Conn) {
+		proxy, err := net.DialTCP("tcp", nil, target)
 		if err != nil {
 			srv.Logger.Print(logger.LOG_LEVEL_ERROR, err)
+			return
 		}
 
-		done := make(chan struct{})
-
-		go func() {
-			defer conn.TCPConn.Close()
-			defer proxyDest.Close()
-
-			io.Copy(proxyDest, conn.TCPConn)
-			done <- struct{}{}
-		}()
-
-		go func() {
-			defer conn.TCPConn.Close()
-			defer proxyDest.Close()
-
-			io.Copy(conn.TCPConn, proxyDest)
-			done <- struct{}{}
-		}()
-
-		<- done
-		<- done
-	}
+		TCPPipe(conn, proxy)
+	}, nil
 }
