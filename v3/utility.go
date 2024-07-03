3c910ef58a79b8aa71b531863b2241da73f84f4f
Alessio Pareto (Desktop-Pare) <alessio.pareto@nixpare.com>
2024-07-03 15:40:10 +0200

    Removed default ciphersuites and curve

--- v3/utility.go
+++ v3/utility.go
@@ -67,29 +67,24 @@ func RandStr(length int, randType CharSet) string {
 	return string(res)
 }
 
+// Used for creating a secure HTTP Server. See the [tls.Config] struct in the standard
+// library for more informations
+var (
+	TLSDefaultCipherSuiteIDs   []uint16
+	TLSDefaultCurvePreferences []tls.CurveID
+)
 
+func init() {
+	for _, suite := range tls.CipherSuites() {
+		TLSDefaultCipherSuiteIDs = append(TLSDefaultCipherSuiteIDs, suite.ID)
+	}
+}
 
 func GenerateTSLConfig(certs []Certificate) (*tls.Config, error) {
 	cfg := &tls.Config{
-		CipherSuites: []uint16{
-			tls.TLS_AES_128_GCM_SHA256,
-			tls.TLS_AES_256_GCM_SHA384,
-			tls.TLS_CHACHA20_POLY1305_SHA256,
-			tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
-			tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
-			tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
-			tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
-			tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305_SHA256,
-			tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305_SHA256,
-			tls.TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA256,
-		},
-		CurvePreferences: []tls.CurveID{
-			tls.CurveP256,
-			tls.CurveP384,
-			tls.X25519,
-		},
-		MinVersion:               tls.VersionTLS12,
-		PreferServerCipherSuites: true,
+		CipherSuites:     TLSDefaultCipherSuiteIDs,
+		CurvePreferences: TLSDefaultCurvePreferences,
+		MinVersion:       tls.VersionTLS12,
 	}
 
 	for _, x := range certs {
